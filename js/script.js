e88676adcb09897a3674a31769f0615e33732e4a
amaljom <amaljom99@gmail.com>
2022-07-04 16:07:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const app = new Vue(
+    
+    {
+        el: "#root",
+        
+        data:{
+            saluto: 'Hello world!',
+            image: "https://img.redbull.com/images/c_crop,x_669,y_0,h_2000,w_1600/c_fill,w_780,h_970/q_auto,f_auto/redbullcom/2021/3/30/j0canri9gk5cpw2f4r1p/beyond-the-lines-s2-e1-art",
+            addClass: "text-center",
+            textUser:'scrivi qualcosa!',
+            addClass2: "mt-3"
+        },
+    
+    }
+);
